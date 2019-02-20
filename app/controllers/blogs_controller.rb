03a9144eef003bf3b27611af5fbd2eece94802e8
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2019-02-20 16:41:23 +0900

    blogs#index 以外ログイン

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except=>[:index]
 
   # GET /blogs
   # GET /blogs.json
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_053656) do
+ActiveRecord::Schema.define(version: 2019_02_14_103013) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "start_time"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2019_01_12_053656) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "pagelink"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
